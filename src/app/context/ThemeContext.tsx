35bae13328643bd5f0fa443d01ab1226afd64f8d
`Faiz-js` <`faiz.momin18112003@gmail.com`>
2025-04-04 16:38:24 +0530

    code pushed

--- src/app/context/ThemeContext.tsx
+++ src/app/context/ThemeContext.tsx
@@ -10,7 +10,7 @@ type ThemeContextType = {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [theme, setTheme] = useState<"light" | "dark">("light");
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {

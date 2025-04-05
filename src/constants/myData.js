6036f25f70d89d36afaf74f537f7f4edb5817274
`Faiz-js` <`faiz.momin18112003@gmail.com`>
2025-04-05 15:39:01 +0530

    code pushed

--- src/constants/myData.js
+++ src/constants/myData.js
@@ -1,6 +1,6 @@
 export const myData = {
   resume:
-    "https://drive.google.com/file/d/1IAO73PnwWT5HvEctj-iCqgm-on6QqIBo/view?usp=drive_link",
+    "https://drive.google.com/file/d/1T7DBoy5Qlu1TwkTW1iEIOfFBm-SedLna/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/faiz-momin-3b5163315/",
     github: "https://github.com/Faiz-js?tab=overview&from=2025-03-01&to=2025-03-15",
     instagram: "https://www.instagram.com/faiz__jod/",
